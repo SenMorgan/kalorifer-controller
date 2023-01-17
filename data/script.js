ffe45066ccaaa78a28dab7a991ab1f5d2daccc05
Sen Morgan <technars@gmail.com>
2023-01-17 23:19:46 +0100

    feat: add water temp gauge

--- data/script.js
+++ data/script.js
@@ -1,85 +1,35 @@
-// Get current sensor readings when the page loads  
+// Get current sensor readings when the page loads
 window.addEventListener('load', getReadings);
 
-// Create Temperature Gauge
-var gaugeTemp = new LinearGauge({
-  renderTo: 'gauge-temperature',
-  width: 120,
-  height: 400,
-  units: "Temperature C",
-  minValue: 0,
-  startAngle: 90,
-  ticksAngle: 180,
-  maxValue: 40,
-  colorValueBoxRect: "#049faa",
-  colorValueBoxRectEnd: "#049faa",
-  colorValueBoxBackground: "#f1fbfc",
-  valueDec: 2,
-  valueInt: 2,
-  majorTicks: [
-      "0",
-      "5",
-      "10",
-      "15",
-      "20",
-      "25",
-      "30",
-      "35",
-      "40"
-  ],
-  minorTicks: 4,
-  strokeTicks: true,
-  highlights: [
-      {
-          "from": 30,
-          "to": 40,
-          "color": "rgba(200, 50, 50, .75)"
-      }
-  ],
-  colorPlate: "#fff",
-  colorBarProgress: "#CC2936",
-  colorBarProgressEnd: "#049faa",
-  borderShadowWidth: 0,
-  borders: false,
-  needleType: "arrow",
-  needleWidth: 2,
-  needleCircleSize: 7,
-  needleCircleOuter: true,
-  needleCircleInner: false,
-  animationDuration: 1500,
-  animationRule: "linear",
-  barWidth: 10,
-}).draw();
-  
-// Create Humidity Gauge
-var gaugeHum = new RadialGauge({
-  renderTo: 'gauge-humidity',
+// Create Water Temperature Gauge
+var gaugeWaterTemp = new RadialGauge({
+  renderTo: 'gauge-water-temperature',
   width: 300,
   height: 300,
-  units: "Humidity (%)",
+  units: "Temperature (C)",
   minValue: 0,
-  maxValue: 100,
+  maxValue: 120,
   colorValueBoxRect: "#049faa",
   colorValueBoxRectEnd: "#049faa",
   colorValueBoxBackground: "#f1fbfc",
   valueInt: 2,
   majorTicks: [
-      "0",
-      "20",
-      "40",
-      "60",
-      "80",
-      "100"
-
+    "0",
+    "20",
+    "40",
+    "60",
+    "80",
+    "100",
+    "120"
   ],
   minorTicks: 4,
   strokeTicks: true,
   highlights: [
-      {
-          "from": 80,
-          "to": 100,
-          "color": "#03C0C1"
-      }
+    {
+      "from": 80,
+      "to": 120,
+      "color": "#ff0006"
+    },
   ],
   colorPlate: "#fff",
   borderShadowWidth: 0,
@@ -92,49 +42,148 @@ var gaugeHum = new RadialGauge({
   colorNeedleCircleOuter: "#007F80",
   needleCircleOuter: true,
   needleCircleInner: false,
-  animationDuration: 1500,
+  animationDuration: 1000,
   animationRule: "linear"
 }).draw();
 
-// Function to get current readings on the webpage when it loads for the first time
-function getReadings(){
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var myObj = JSON.parse(this.responseText);
-      console.log(myObj);
-      var temp = myObj.temperature;
-      var hum = myObj.humidity;
-      gaugeTemp.value = temp;
-      gaugeHum.value = hum;
+// Create Temperature Gauge
+var gaugeAirTemp = new LinearGauge({
+  renderTo: 'gauge-air-temperature',
+  width: 120,
+  height: 400,
+  units: "Temperature (C)",
+  minValue: 0,
+  startAngle: 90,
+  ticksAngle: 180,
+  maxValue: 40,
+  colorValueBoxRect: "#049faa",
+  colorValueBoxRectEnd: "#049faa",
+  colorValueBoxBackground: "#f1fbfc",
+  valueDec: 2,
+  valueInt: 2,
+  majorTicks: [
+    "0",
+    "5",
+    "10",
+    "15",
+    "20",
+    "25",
+    "30",
+    "35",
+    "40"
+  ],
+  minorTicks: 4,
+  strokeTicks: true,
+  highlights: [
+    {
+      "from": 30,
+      "to": 40,
+      "color": "rgba(200, 50, 50, .75)"
     }
-  }; 
+  ],
+  colorPlate: "#fff",
+  colorBarProgress: "#CC2936",
+  colorBarProgressEnd: "#049faa",
+  borderShadowWidth: 0,
+  borders: false,
+  needleType: "arrow",
+  needleWidth: 2,
+  needleCircleSize: 7,
+  needleCircleOuter: true,
+  needleCircleInner: false,
+  animationDuration: 1000,
+  animationRule: "linear",
+  barWidth: 10,
+}).draw();
+
+// Create Humidity Gauge
+var gaugeAirHum = new RadialGauge({
+  renderTo: 'gauge-air-humidity',
+  width: 300,
+  height: 300,
+  units: "Humidity (%)",
+  minValue: 0,
+  maxValue: 100,
+  colorValueBoxRect: "#049faa",
+  colorValueBoxRectEnd: "#049faa",
+  colorValueBoxBackground: "#f1fbfc",
+  valueInt: 2,
+  majorTicks: [
+    "0",
+    "20",
+    "40",
+    "60",
+    "80",
+    "100"
+
+  ],
+  minorTicks: 4,
+  strokeTicks: true,
+  highlights: [
+    {
+      "from": 80,
+      "to": 100,
+      "color": "#03C0C1"
+    }
+  ],
+  colorPlate: "#fff",
+  borderShadowWidth: 0,
+  borders: false,
+  needleType: "line",
+  colorNeedle: "#007F80",
+  colorNeedleEnd: "#007F80",
+  needleWidth: 2,
+  needleCircleSize: 3,
+  colorNeedleCircleOuter: "#007F80",
+  needleCircleOuter: true,
+  needleCircleInner: false,
+  animationDuration: 1000,
+  animationRule: "linear"
+}).draw();
+
+
+function setValues(myObj) {
+  console.log(myObj);
+  gaugeAirTemp.value = myObj.air_temp;
+  gaugeAirHum.value = myObj.air_hum;
+  gaugeWaterTemp.value = myObj.water_temp;
+  relay_state = myObj.relay_state;
+  document.getElementById("relay_state_id").innerHTML = relay_state ? "ON" : "OFF";
+}
+
+// Function to get current readings on the webpage when it loads for the first time
+function getReadings() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var jsonResponse = JSON.parse(this.responseText);
+      setValues(jsonResponse);
+    }
+  };
   xhr.open("GET", "/readings", true);
   xhr.send();
 }
 
 if (!!window.EventSource) {
   var source = new EventSource('/events');
-  
-  source.addEventListener('open', function(e) {
+
+  source.addEventListener('open', function (e) {
     console.log("Events Connected");
   }, false);
 
-  source.addEventListener('error', function(e) {
+  source.addEventListener('error', function (e) {
     if (e.target.readyState != EventSource.OPEN) {
       console.log("Events Disconnected");
     }
   }, false);
-  
-  source.addEventListener('message', function(e) {
+
+  source.addEventListener('message', function (e) {
     console.log("message", e.data);
   }, false);
-  
-  source.addEventListener('new_readings', function(e) {
+
+  source.addEventListener('new_readings', function (e) {
     console.log("new_readings", e.data);
-    var myObj = JSON.parse(e.data);
-    console.log(myObj);
-    gaugeTemp.value = myObj.temperature;
-    gaugeHum.value = myObj.humidity;
+    var jsonResponse = JSON.parse(e.data);
+    setValues(jsonResponse);
   }, false);
 }
